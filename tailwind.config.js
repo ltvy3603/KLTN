219624b8c0e719a5c777a14fee84dcd8ff9c21c6
Lê Tường Vy <letvy3603@gmail.com>
2024-12-14 14:32:04 +0700

    14:31 ngay 14/12/2024

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import plugin from 'tailwindcss/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export const content = ['./index.html', './src/**/*.{js,ts,jsx,tsx}'];
+export const corePlugins = {
+  container: false
+};
+export const theme = {
+  extend: {
+    colors: {
+      orange: '#ee4d2d'
+    }
+  }
+};
+export const plugins = [
+  plugin(function ({ addComponents, theme }) {
+    addComponents({
+      '.container': {
+        maxWidth: theme('columns.7xl'),
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        paddingLeft: theme('spacing.4'),
+        paddingRight: theme('spacing.4')
+      }
+    });
+  })
+];
